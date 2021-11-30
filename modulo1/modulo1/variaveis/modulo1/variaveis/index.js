5342ad64b02b20e13469032d917577c9682792fc
Jamille Reddin <jamillechreddin@gmail.com>
2021-11-29 23:44:04 -0300

    exercicios/variaveis

---
+++ modulo1/modulo1/variaveis/modulo1/variaveis/index.js
@@ -0,0 +1,81 @@
+// 1. Será impresso: 10, 10. 10, 5.
+
+// 2. 10, 10, 10.
+
+// 3. 
+
+// let horasTrabalhadas = prompt("Quantas horas você trabalha por dia?")
+// let ganhoDiario = prompt("Quanto você recebe por dia?")
+// alert(`Voce recebe ${horasTrabalhadas/ganhoDiario} por hora`)
+
+// Exercicio de escrita de Código
+
+// 1. 
+
+let nomeDoUsuario
+let idadeDoUsuario
+
+typeof nomeDoUsuario
+typeof idadeDoUsuario
+
+console.log(nomeDoUsuario, idadeDoUsuario)
+
+// d) Reflita: por que esse tipo foi impresso? 
+// Foi impresso unfefined pois não foi atribuído nenhum valor a variável, por ser uma let
+// é possivel alterar esse valor mais tarde.
+
+let seunome= prompt("Qual o seu nome?")
+let suaidade = prompt("Qual a sua idade?")
+
+console.log("Olá", seunome, "você tem", suaidade, "de idade.")
+
+// F.  Novamente, imprima na tela o tipo dessas variáveis. O que você notou? 
+// notei que por ser "let" é possivel alterar e o valor será impresso de acordo 
+// com o que for colocado pelo usuário.
+
+
+2.
+
+let usaVerde = prompt("Você está usando verde?")
+typeof usaVerde 
+let ouveMusica =prompt("Você ouve música todo dia?")
+typeof ouveMusica 
+let estudouHoje = prompt("Estudou algo hoje?")
+typeof estudouHoje 
+
+
+console.log("Você está usando verde?", usaVerde)
+console.log("Você ouve música todo dia?", ouveMusica)
+console.log("Estudou algo hoje?", estudouHoje)
+
+// 3. 
+
+let a = 10
+let b = 25
+let c = a
+
+a=b
+b=c
+c=a
+
+console.log("O novo valor de a é", a)
+console.log("O novo valor de b é", b) 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
