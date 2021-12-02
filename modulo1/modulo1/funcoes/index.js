31248de8d0ad63efefddea6b9c72b07f8381f3dd
Jamille Reddin <jamillechreddin@gmail.com>
2021-12-02 18:27:21 -0300

    exercicio/funcoes

---
+++ modulo1/modulo1/funcoes/index.js
@@ -0,0 +1,49 @@
+
+// 1. 
+// a) o resultado da multiplicação 5x os valores da função. 10, 50.
+// b) não apareceria nada no console.
+
+// 2.
+// a) Explique o que essa função faz e qual é sua utilidade.
+// Se tiver "cenoura" na frase retorna true e se nao tiver retorna false.
+// b) i. true
+// ii. false
+// iii. false
+
+// -----------------------------------------------------------------------------------------------
+
+// Exercicio de Escrita de Código
+
+// 1.
+// a)
+const meuNome = "Jamille"
+const minhaIdade = 26
+const moroEm = "Joinville"
+const profissao = "estudante" 
+
+function sobreMim(){   }
+
+console.log(`Eu sou ${meuNome}, tenho ${minhaIdade} anos, moro em ${moroEm} e sou ${profissao}.`)
+
+// b)
+
+const seuNome = "Luiza"
+const suaIdade= 24
+const ondeMora = "Curitiba"
+const suaProfissao = "Atriz"
+
+
+function imprimirFrase(textoTemplate){
+const textoTemplate = `Eu sou ${seuNome}`
+
+}
+
+console.log(textoTemplate)
+
+
+
+
+
+
+
+ 
